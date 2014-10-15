a2db1c529407af213eaf551250835d883a5e11a2
unknown <vaibhavpandey5388@gmail.com>
2014-10-15 23:40:05 +0530

    more excercises

---
+++ Q40.sql
@@ -0,0 +1,22 @@
+/*
+
+Short database description "Ships":
+
+The database of naval ships that took part in World War II is under consideration. The database has the following relations: 
+Classes(class, type, country, numGuns, bore, displacement) 
+Ships(name, class, launched) 
+Battles(name, date) 
+Outcomes(ship, battle, result) 
+Ships in classes are arranged to a single project. A class is normally assigned the name of the first ship in the class under consideration (head ship); otherwise, the class name does not coincide with any ship name in the database. 
+The Classes relation includes the class name, type (bb for a battle ship, or bc for a battle cruiser), country where the ship was built, number of main guns, gun caliber (diameter of the gun barrel, in inches), and displacement (weight in tons). The Ships relation includes the ship name, its class name, and launch year. The Battles relation covers the name and date of a battle the ships participated; while the result of their participation in the battle (sunk, damaged, or unharmed - OK) is in the Outcomes relation. 
+Notes: 1) Outcomes relation may include the ships not included in the Ships relation. 2) Sunk ship can not participate in battles after that.
+
+Question: Find the class, name and country for the ships in Ships table, which have not less than 10 guns.
+
+*/
+
+Select a.class, a.name, b.country
+from ships a
+inner join classes b
+on a.class = b.class
+and numguns >= 10
